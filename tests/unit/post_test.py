f03f23eedcf9bb9ae4e9f0bd8dbc2981f8374780
kokiwadas <koki.wadas@gmail.com>
2019-08-20 13:55:16 +0200

    Added test to check if json dictionary in Post object was created in proper way.

--- tests/unit/post_test.py
+++ tests/unit/post_test.py
@@ -6,6 +6,12 @@ class PostTest(TestCase):
     def test_create_post(self):
         p = Post('Test', 'Test Content')
 
-        self.assertEqual('Testx', p.title)
+        self.assertEqual('Test', p.title)
         self.assertEqual('Test Content', p.content)
+
+    def test_json(self):
+        p = Post('Test', 'Test Content')
+        expected = {'title': 'Test', 'content': 'Test Content'}
+
+        self.assertDictEqual(expected, p.json())
 
