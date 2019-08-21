417d27557fd9f60f1c47d4d1a2d86fb374e37326
kokiwadas <koki.wadas@gmail.com>
2019-08-21 11:53:34 +0200

    Added method to create posts in Blog.

--- blog.py
+++ blog.py
@@ -1,3 +1,5 @@
+from post import Post
+
 
 class Blog:
     def __init__(self, title, author):
@@ -7,3 +9,6 @@ class Blog:
 
     def __repr__(self):
         return f'{self.title} by {self.author} ({len(self.posts)} post{"s" if len(self.posts) != 1 else ""})'
+
+    def create_post(self, title, content):
+        self.posts.append(Post(title, content))
