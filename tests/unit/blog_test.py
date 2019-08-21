67f08ddf7dcb13a6358cd3a1d5736d5b7db7830c
kokiwadas <koki.wadas@gmail.com>
2019-08-21 11:35:46 +0200

    Updated test to check if Blog object with multiple posts is created correctly.

--- tests/unit/blog_test.py
+++ tests/unit/blog_test.py
@@ -18,4 +18,7 @@ class BlogTest(TestCase):
     def test_repr_multiple_posts(self):
         b = Blog('Test', 'Test Author')
         b.posts = ['test']
+        b2 = Blog('Test_1', 'Test Author Two')
+        b2.posts = ['test', 'test1']
         self.assertEqual(b.__repr__(), 'Test by Test Author (1 post)')
+        self.assertEqual(b2.__repr__(), 'Test_1 by Test Author Two (2 posts)')
