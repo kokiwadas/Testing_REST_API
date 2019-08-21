07f104a477372cd4703d2885e263c3b8473b0b85
kokiwadas <koki.wadas@gmail.com>
2019-08-21 12:26:43 +0200

    Added jason representation for Blog object.

--- blog.py
+++ blog.py
@@ -12,3 +12,10 @@ class Blog:
 
     def create_post(self, title, content):
         self.posts.append(Post(title, content))
+
+    def json(self):
+        return {
+            'title': self.title,
+            'author': self.author,
+            'posts': [post.json() for post in self.posts]
+        }
